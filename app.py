b15dbc67179adf2e319f9e8abd87ae035c38cb06
laurabeudin2003-coder <laurabeudin2003@gmail.com>
2025-10-21 09:33:45 +0200

    Update app.py

--- app.py
+++ app.py
@@ -11,3 +11,19 @@ d = {"nombres" : nombres, "carré" : carre}
 data = pd.DataFrame(d)
 
 st.dataframe(data)
+
+st.header("Widgets Interactifs")
+
+# Un slider
+age = st.slider("Quel est votre âge ?", 0, 100, 25)
+st.write("Votre âge est :", age)
+
+# Une liste de sélection
+option = st.selectbox(
+    'Quelle est votre couleur préférée ?',
+    ('Bleu', 'Rouge', 'Vert'))
+st.write('Votre couleur préférée est :', option)
+
+# Un bouton
+if st.button('Cliquez ici !'):
+    st.write('Vous avez cliqué ! Bravo !')
