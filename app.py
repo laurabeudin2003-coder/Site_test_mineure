e4b02a64b415d6e67be0315f72fcbe4a004e06ad
laurabeudin2003-coder <laurabeudin2003@gmail.com>
2025-10-21 09:30:40 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,12 @@
 import streamlit as st
 
 st.header("Ma première application !")
-
 st.write("Mineure numérique - Laura")
+
+nombres = [1, 2, 4, 7]
+carre = [1**2, 2**2, 4**2, 7**2]
+
+d = {"nombres" : nombres, "carré" : carre}
+data = pd.DataFrame(d)
+
+st.dataframe(data)
